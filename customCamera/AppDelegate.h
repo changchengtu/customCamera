a356b4b5d9448f35aca0d945ff411f27700033ca
C.C.T <changcheng.tu@gmail.com>
2014-09-05 13:44:09 +0800

    Initial Commit

---
+++ customCamera/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  customCamera
+//
+//  Created by 杜長城 on 9/5/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ customCamera/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  customCamera
+//
+//  Created by 杜長城 on 9/5/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
